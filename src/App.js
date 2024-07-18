4fd571a93462248d2bc8086998f071e86afc2a15
Ammar Hassan <ammar@ammarhassan.co.uk>
2024-07-18 18:06:57 +0100

    update to file, removed bootstraped

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import Board from './Board.js'
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+     <section>
+        <h1>Kanban</h1>
+        <div class="main__container">
+        <Board title="In Progress" />
+        <Board title="In Testing" />
+        <Board title="Deployed" />
+        </div>
+     </section>
     </div>
   );
 }

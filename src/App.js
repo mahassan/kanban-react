e8a79513eff83e7d30b3192dd167c276d5893c12
Ammar Hassan <ammar@ammarhassan.co.uk>
2024-07-20 14:33:11 +0100

    props for details on each card

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
      <section>
         <h1>Kanban</h1>
         <div class="main__container">
-        <Board title="In Progress" />
-        <Board title="In Testing" />
-        <Board title="Deployed" />
+        <Board title="In Progress" details="To add a CTA to the kanban button" />
+        <Board title="In Testing" details="Nothing in testing" />
+        <Board title="Deployed" details="Deployed" />
         </div>
      </section>
     </div>

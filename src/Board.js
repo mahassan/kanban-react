b8d25b72e6085544637afc1d96c81b22f83d34ba
Ammar Hassan <ammar@ammarhassan.co.uk>
2024-07-20 14:33:32 +0100

    displaying data from props for card

--- src/Board.js
+++ src/Board.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import Card from './Cards/index.js'
 const Board = (props) => {
     return (
        <div>
          <h2>{props.title}</h2>
+         <div className='card__column'>
+            <Card details={props.details}/>
+         </div>
        </div>
     );
 };
